4ab93ede09164a22eabcd6c20b0811ac87bbf6a3
Ankhena <ankhena@ya.ru>
2026-02-06 16:02:48 +0300

    Edit full-year.js

--- source/scripts/utils/full-year.js
+++ source/scripts/utils/full-year.js
@@ -3,13 +3,17 @@
 
 const yearBlock = document.querySelector('[data-copyright]');
 const startYearBlock = document.querySelector('[data-startYear]');
-const startYear = startYearBlock.innerText;
-const currentYear = new Date().getFullYear();
-if (startYear < currentYear) {
-  yearBlock.innerHTML = `© ${startYear} - ${currentYear}`;
-} else {
-  yearBlock.innerHTML = `© ${startYear}`;
+
+if (yearBlock && startYearBlock) {
+  const startYear = startYearBlock.innerText;
+  const currentYear = new Date().getFullYear();
+  if (startYear < currentYear) {
+    yearBlock.innerHTML = `© ${startYear} - ${currentYear}`;
+  } else {
+    yearBlock.innerHTML = `© ${startYear}`;
+  }
 }
+
 
 // full-year for copyright
 //////////////////////////////////////////////////////////////////
